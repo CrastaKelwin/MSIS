8562bf5531cee56a35fdd41db905993d76417fcd
Kelwin Crasta <crastakelwin@gmail.com>
2023-11-13 19:32:44 +0530

    added proteus and updated keil project

--- MCA/1_LED_Blink/LED blink.c
+++ MCA/1_LED_Blink/LED blink.c
@@ -1,4 +1,6 @@
 #include <lpc214x.h>
+#include "../pll/pll.h"
+#include "../timer/timer.h"
 
 // bit_config.h file include
 // |- MCA
@@ -11,18 +13,12 @@
 
 // LEDs Connected to Port P0.0 - P0.7
 
-void delay()
-{
-	int i = 0, j = 0;
-	for (i = 0; i < 10000; i++)
-	{
-	}
-}
-
 int main(void)
 {
 	// Set Port P0.0 - P0.7 as GPIO function
 	PINSEL0 &= ~0xffff;
+	// Init PLL for 61MHz at 12MHz osc
+	PLL_Init_60MHz();
 	// Set Port P0.0 - P0.7 as Output
 	SET_REG_BITS(IO0DIR, 0, 7); // Expands to IO0DIR |= 0xff;
 	while (1)
@@ -30,10 +26,10 @@ int main(void)
 		// Set Port P0.0 - P0.7 as HIGH
 		SET_REG_BITS(IO0SET, 0, 7); //  Expands to IO0SET |= 0xff;
 		// Delay of approx 1 second
-		delay();
+		delay_ms_timer0(1000);
 		// Clear Port P0.0 - P0.7 as LOW
 		SET_REG_BITS(IO0CLR, 0, 7); //  Expands to IO0CLR |= 0xff;
 		// Delay of approx 1 second
-		delay();
+		delay_ms_timer0(1000);
 	}
 }
